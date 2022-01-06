8ea8614a95609b6c24748eaf4df67fe0adc86a48
Shuoshuo <liqiangge628@gmail.com>
2022-01-06 10:39:02 +0800

    first commit

---
+++ src/main/java/com/Turing/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.Turing.git;
+
+/**
+ * @author Turing
+ * @date 2022/1/6 10:34
+ **/
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
